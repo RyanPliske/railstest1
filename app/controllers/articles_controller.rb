bb7dc8431af3e04ecc2fc6e8f0b5feee2caeaf95
Ryan Pliske <ryanpliske@yahoo.com>
2015-03-24 17:06:03 -0500

    Added Validation and Error Handling
    Article Model, validates of the title is at least 5 characters long. If
    it’s less than 5, it sends it back to the Articles Controller where the
    controller tells the view to display ‘new’ form again and this time
    you’ll see the errors that occurred.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,27 +1,34 @@
 class ArticlesController < ApplicationController
-	def new
 
+	def index
+		@articles = Article.all
 	end
 
 	def show
 		@article = Article.find(params[:id])
-		redirect_to @article
+	end
+
+	def new
+		@article = Article.new
 	end
 
 	def create
 		' params Array contains parameters (KVP) from URL : Key = plain, value = params[:article]'
 		' params[:article] = "whatever" for http://www.example.com/?article=whatever'
 		' render plain: params[:article].inspect '
-		'Save parameters to Article Model. (classnames begin with capital letter)'
+		' Save parameters to Article Model. (classnames begin with capital letter)'
 		@article = Article.new(article_params)
-		@article.save
-		' http://api.rubyonrails.org/classes/ActionController/Redirecting.html'
-		' rails passes instance variables to the view (instance var @)'
-		redirect_to @article
-	end
 
-	def index
-		@articles = Article.all
+		if @article.save
+			' http://api.rubyonrails.org/classes/ActionController/Redirecting.html'
+			' rails passes instance variables to the view (instance var @)'
+			redirect_to @article
+		else
+			' @article object is passed back to the new template when its rendered.'
+			' This rendering is done on the same request as form submission. VS redirect_to tells 
+				the browser to issue another request '
+			render 'new'
+		end
 	end
 
 	' private method to Whitelist controller parameters to prevent wrongful assignment in model'
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ActiveRecord::Base
+	' Ensure title is at least 5 characters long '
+	validates :title, presence: true, length: { minimum: 5 }
 end
