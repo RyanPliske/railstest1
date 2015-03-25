196b4ec08ba6f010669d65fe3835a8ef47625285
Ryan Pliske <ryanpliske@yahoo.com>
2015-03-25 13:04:59 -0500

    Create Comments
    *migrate to add new comments table in database
    *show comments
    *comments controller adds comment to article

--- db/schema.rb
+++ db/schema.rb
@@ -23,8 +23,11 @@ ActiveRecord::Schema.define(version: 20150325162807) do
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
     t.text     "body"
+    t.integer  "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
 end
