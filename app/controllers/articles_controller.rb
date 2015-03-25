7e61efcd1bcf5b5c8c31fdb968ba2d897b69f054
Ryan Pliske <ryanpliske@yahoo.com>
2015-03-25 13:43:43 -0500

    Added basic auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+	http_basic_authenticate_with name: "ryan", password: "secret", except: [:index, :show]
+
 	def new
 		'Send @article to view'
 		@article = Article.new
